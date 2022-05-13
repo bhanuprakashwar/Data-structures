14bf488a91e97caf8a12c9c4a4c0174b58e7391a
Bhanu <likebhanuprakash@gmail.com>
2022-05-13 18:50:38 +0530

    added Binary tree

---
+++ Tree/Binary-Tree.ts
@@ -0,0 +1,56 @@
+
+class TreeNode {
+    left: TreeNode | null;
+    right: TreeNode | null;
+    data: number;
+    constructor(value: number) {
+        this.data = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function insertNode(node: TreeNode | null, data: number): TreeNode {
+    if (node === null) return new TreeNode(data);
+    if (data < node.data) {
+        node.left = insertNode(node.left, data);
+    } else {
+        node.right = insertNode(node.right, data);
+    }
+    return node;
+}
+
+function inorder(node: TreeNode | null, ans: number[]) {
+    if (node !== null) {
+        inorder(node.left, ans);
+        ans.push(node.data);
+        inorder(node.right, ans);
+    }
+}
+function preorder(node: TreeNode | null, ans: number[]) {
+    if (node !== null) {
+        ans.push(node.data);
+        preorder(node.left, ans);
+        preorder(node.right, ans);
+    }
+}
+function postorder(node: TreeNode | null, ans: number[]) {
+    if (node !== null) {
+        postorder(node.left, ans);
+        postorder(node.right, ans);
+        ans.push(node.data);
+    }
+}
+
+let root: TreeNode | null = null;
+root = insertNode(root, 3);
+root = insertNode(root, 1);
+root = insertNode(root, 2);
+root = insertNode(root, 4);
+root = insertNode(root, 7);
+root = insertNode(root, 5);
+root = insertNode(root, 6);
+
+let ans: number[] = []
+inorder(root, ans)
+console.log(ans);
